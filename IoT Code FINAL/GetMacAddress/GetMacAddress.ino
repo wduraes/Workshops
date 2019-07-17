006133f9ae08e77c08b9d2b1e66601942ac8ce4d
wduraes <wellington.duraes@gmail.com>
2019-07-17 16:06:22 -0700

    New and improved method

--- IoT Code FINAL/GetMacAddress/GetMacAddress.ino
+++ IoT Code FINAL/GetMacAddress/GetMacAddress.ino
@@ -5,26 +5,17 @@
 */
 
 #include <ESP8266WiFi.h>
-
-uint8_t MAC_array[6];
-char MAC_char[18];
-
-void setup() 
-{
-  Serial.begin(115200);
-  Serial.println();
-
-  WiFi.macAddress(MAC_array);
-  for (int i = 0; i < sizeof(MAC_array); ++i)
-  {
-    sprintf(MAC_char,"%s%02x:",MAC_char,MAC_array[i]);
-  }
-  
-  Serial.println(MAC_char);
+ 
+void setup(){ 
+   Serial.begin(115200);
+   delay(500); 
 }
-
-void loop() {
-  
+ 
+void loop(){ 
+  Serial.println();
+  Serial.print("MAC: ");
+  Serial.println(WiFi.macAddress());
+  delay(2000);
 }
 
 
