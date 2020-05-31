f5731decd64b91180dbd5b31af3d0552508a23ef
Wellington Duraes <wduraes@microsoft.com>
2020-05-31 09:51:44 -0700

    fixing interrupt code

--- Arduino 102/Interrupts/Interrupts.ino
+++ Arduino 102/Interrupts/Interrupts.ino
@@ -1,7 +1,7 @@
 //Interrupt Example
 #define LED 6
 #define BTN 2
-int Delay_Time = 1000;
+volatile int Delay_Time = 1000;
 
 void setup()
 {
@@ -10,7 +10,7 @@ void setup()
   
   // Attach an interrupt to pin 2, on the rising edge, 
   // calling a function named Interrupt_Handling 
-  attachInterrupt(0, Interrupt_Handling, RISING);
+  attachInterrupt(digitalPinToInterrupt(0), Interrupt_Handling, RISING);
 }
 
 void loop()
