01cb83498f2478eb5579959adb55c89d1e39f6b3
Pan <43349657+hkblock@users.noreply.github.com>
2021-03-20 23:48:53 -0700

    Temperature conversion, light constrainment
    - Added optional Celcius-to-Fahrenheit conversion that can be uncommented
    - Mapped light measurement to three characters so it always fits onscreen

--- Arduino Barebones/Part_2_Additional_Components/Sensor-data/Sensor-data.ino
+++ Arduino Barebones/Part_2_Additional_Components/Sensor-data/Sensor-data.ino
@@ -40,13 +40,23 @@ void loop()
   
   if(!isnan(event.temperature))  
   {
-    lcd.print(event.temperature);  
+    float tempMeasurement = event.temperature;
+    
+    // The measurement is in Celcius. If you want it in Fahrenheit,
+    // uncomment the following line:
+    // tempMeasurement = (event.temperature * 1.8) + 32;
+    
+    lcd.print(tempMeasurement);  
   } 
 
   lcd.setCursor(10, 0);
   lcd.print("L:");
   lcd.setCursor(13, 0);  //set the cursor to the 4th column, second line
-  lcd.print(analogRead(A1));  
+  
+  // The light value gets printed in three characters. If there's a really
+  // bright light, the fourth digit is off-screen on a 2x16 LCD.
+  int light = map(analogRead(A1), 0, 1023, 0, 999);
+  lcd.print(light);  
 
   lcd.setCursor(0, 1);
   lcd.print("H:");
